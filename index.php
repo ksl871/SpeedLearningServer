37b9aeb556a05db7424bfb8141ad1960db39c1c9
Alex Keng <ksl871@gmail.com>
2014-10-22 07:00:49 -0700

    reading from db

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello World";
+
+require_once 'config.php';
+
+try {
+    $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+    $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+    $oResult = $oStmt->fetchAll();
+
+    foreach ($oResult as $aRow) {
+        print_r($aRow['data']);
+    }
+
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
 ?>
